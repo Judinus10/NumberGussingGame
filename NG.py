901f57cee06c68695f30471e890e5dd03426668f
judinus10 <judes.20222403@iit.ac.lk>
2024-08-17 15:54:02 +0530

    add some limitation for gussing

--- NG.py
+++ NG.py
@@ -5,12 +5,15 @@ num = random.randint(1, 100)# Generating a random number between 1 and 100
 guess = int(input("Guess a number between 1 to 100: "))# Asking the user to guess the number
 
 # Loop until the user guesses the correct number
-while num != guess:
+i=0 
+
+while i<3:
     if num > guess:
         print("Your guess is too low!")
     else:
         print("Your guess is too high!")
 
-    guess = int(input("Guess again: "))# Prompt the user to guess again
+    guess = int(input("You have only "+i+" chances Guess again: "))# Prompt the user to guess again
+    i+=1
 
 print("Congrats!!! \nYou guessed it right!")# Congratulate the user when they guess correctly
