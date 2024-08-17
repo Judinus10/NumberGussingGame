45bed59649859b6b68472f9cee98bf5058782cc4
judinus10 <judes.20222403@iit.ac.lk>
2024-08-17 15:39:19 +0530

    ask user for input

--- NG.py
+++ NG.py
@@ -1,3 +1,5 @@
 import random
-# Generating a random number between 1 and 100
-num = random.randint(1, 100)
+
+num = random.randint(1, 100)# Generating a random number between 1 and 100
+
+guess = int(input("Guess a number between 1 to 100: "))# Asking the user to guess the number
