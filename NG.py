68d64ec894624f5bd870f757d31036c58f8fc1b3
judinus10 <judes.20222403@iit.ac.lk>
2024-08-17 15:57:39 +0530

    add some changes

--- NG.py
+++ NG.py
@@ -1,19 +1,28 @@
 import random
 
-num = random.randint(1, 100)# Generating a random number between 1 and 100
+# Generating a random number between 1 and 100
+num = random.randint(1, 100)
 
-guess = int(input("Guess a number between 1 to 100: "))# Asking the user to guess the number
+# Asking the user to guess the number
+guess = int(input("Guess a number between 1 to 100: "))
 
-# Loop until the user guesses the correct number
-i=0 
+# Loop to allow the user 3 chances to guess
+i = 0 
 
-while i<3:
+while i < 3:
     if num > guess:
         print("Your guess is too low!")
-    else:
+    elif num < guess:
         print("Your guess is too high!")
+    else:
+        break  # Exit the loop if the guess is correct
+    
+    i += 1
+    if i < 3:  # Check if there are any attempts left
+        guess = int(input(f"You have {3 - i} chances left. Guess again: "))
 
-    guess = int(input("You have only "+i+" chances Guess again: "))# Prompt the user to guess again
-    i+=1
-
-print("Congrats!!! \nYou guessed it right!")# Congratulate the user when they guess correctly
+# Check if the user guessed the number correctly
+if num == guess:
+    print("Congrats!!! \nYou guessed it right!")
+else:
+    print(f"Oh! The correct number was {num}.\nBetter luck next time!")
