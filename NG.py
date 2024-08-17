4d92eb0c8f649bf6d50f362d6ef47d730bf14297
judinus10 <judes.20222403@iit.ac.lk>
2024-08-17 15:39:59 +0530

    loop untill guess the number

--- NG.py
+++ NG.py
@@ -3,3 +3,10 @@ import random
 num = random.randint(1, 100)# Generating a random number between 1 and 100
 
 guess = int(input("Guess a number between 1 to 100: "))# Asking the user to guess the number
+
+# Loop until the user guesses the correct number
+while num != guess:
+    if num > guess:
+        print("Your guess is too low!")
+    else:
+        print("Your guess is too high!")
